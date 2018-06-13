07869e1bbccb48d204e5c1ae3ba12b898cca401c
Robin Bastien <robin@machine-agency.com>
2018-06-13 13:26:12 -0400

    Added a new impact area style and animated typography

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,25 +9,53 @@
 ready(initialize);
 
 var s = Snap("#hero-unit__typography");
+
 // Handle the tagline animation
 function initialize_tagline() {
-  Snap.load("assets/images/tagline2.svg", animate_tagline );
+  Snap.load("assets/images/tagline3.svg", animate_tagline );
 
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
 }
 
 function animate_tagline( data ) {
   s.append( data );
-  var goo_start = Snap.select('#secondary-overlay');
-  var goo_end = Snap.select('#tertiary-overlay');
-  var goo_startPoints = goo_start.node.getAttribute('d');
-  var goo_endPoints = goo_end.node.getAttribute('d');
-  var toFancy = function(){
-    goo_start.animate({ d: goo_endPoints }, 6000, mina.linear, toSimple);
+  var blobs = Snap.select('#Inner-Blobs > g');
+  var outer_blobs = Snap.selectAll('#Outer-Blobs path');
+
+  blobs.animate({"transform" : "r0 600 -50 s1.5"}, 16000);
+  outer_blobs.forEach( (element, i ) => {
+    console.log(element.attr('s') );
+    element.attr('s', getRandomInt(15)/10);
+    console.log(element.attr('s') );
+    element.animate({"transform" : "r0 600 -50 s1.5"}, 16000);
+  });
+
+  function conveyor(el,speed,reversed) {
+    var speed    = speed    || 1,
+        reversed = reversed || 0;
+    var len = reversed ? -el.getTotalLength() : el.getTotalLength(),
+        dur = 60000/speed;
+    el.attr('stroke-dashoffset',0);
+    el.animate({ 'stroke-dashoffset': len }, dur );
+    setTimeout(function() { conveyor(el, reversed); }, dur);
   }
-  var toSimple = function(){
-    goo_start.animate({ d: goo_startPoints }, 6000, mina.linear, toFancy);
-  }
-  toSimple();
+
+  // var goo_end = Snap.select('#tertiary-overlay');
+  // var goo_startPoints = goo_start.node.getAttribute('d');
+  // var goo_endPoints = goo_end.node.getAttribute('d');
+  // var timeout = 1000;
+  //
+  // var toFancy = function(){
+  //   goo_start.animate({ d: goo_endPoints }, timeout, mina.linear, toSimple);
+  // }
+  // var toSimple = function(){
+  //   goo_start.animate({ d: goo_startPoints }, timeout, mina.linear, toFancy);
+  //   timeout = 6500;
+  // }
+  // toSimple();
 
 }
 
